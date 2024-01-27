02ed2e1472541dd8c5a50dc694ead94fefb37600
David J. Hwang <jeongjaehwang@gmail.com>
2024-01-27 22:53:18 +0900

    Add more properties inside data.js data schema

--- data.js
+++ data.js
@@ -2,9 +2,11 @@
 // PHR: Parking-Household Ratio, 세대당 주차대수
 const data = [
   {
-    name: "상계주공2단지",
-    category: "아파트",
+    category: "매매",
+    rating: 5,
 
+    name: "상계주공2단지",
+    categoryEstate: "아파트",
     jibunAddress: "서울시 노원구 상계동 746-5",
     roadAddress: "서울시 노원구 동일로 1355",
 
@@ -18,21 +20,23 @@ const data = [
     wolsePriceMax: "500/55",
     jmr: 50,
 
-    dateWritten: "2024-01-26",
-
     numHousehold: 2029,
     floor: undefined,
     dateApproval: "1987-11-21",
     age: undefined,
     numParking: 1004,
     phr: 0.49,
-
+    
     memo: undefined,
+
+    dateWritten: "2024-01-26",
   },
   {
-    name: "창동주공18단지",
-    category: "아파트",
+    category: "매매",
+    rating: 5,
 
+    name: "창동주공18단지",
+    categoryEstate: "아파트",
     jibunAddress: "서울시 도봉구 창동 30",
     roadAddress: "서울시 도봉구 노해로70길 119",
 
@@ -46,21 +50,23 @@ const data = [
     wolsePriceMax: "1000/58",
     jmr: 32,
 
-    dateWritten: "2024-01-26",
-
     numHousehold: 910,
     floor: undefined,
     dateApproval: "1988-10-26",
     age: undefined,
     numParking: 634,
     phr: 0.69,
-
+    
     memo: undefined,
+
+    dateWritten: "2024-01-26",
   },
   {
-    name: "목련2단지",
-    category: "아파트",
+    category: "매매",
+    rating: 5,
 
+    name: "목련2단지",
+    categoryEstate: "아파트",
     jibunAddress: "경기도 안양시 동안구 호계동 1052",
     roadAddress: "경기도 안양시 동안구 동안로 102",
 
@@ -74,21 +80,23 @@ const data = [
     wolsePriceMax: "1000/70",
     jmr: 36,
 
-    dateWritten: "2024-01-26",
-
     numHousehold: 994,
     floor: undefined,
     dateApproval: "1992-03-28",
     age: undefined,
     numParking: 994,
     phr: 1,
-
+    
     memo: undefined,
+
+    dateWritten: "2024-01-26",
   },
   {
-    name: "가산양우내안애애플 1동",
-    category: "오피스텔",
+    category: "매매",
+    rating: 5,
 
+    name: "가산양우내앙애애플1동",
+    categoryEstate: "오피스텔",
     jibunAddress: "서울시 금천구 가산동 141-2",
     roadAddress: "서울시 금천구 남부순환로 1266",
 
@@ -102,21 +110,23 @@ const data = [
     wolsePriceMax: "1000/60",
     jmr: 95,
 
-    dateWritten: "2024-01-26",
-
     numHousehold: 92,
     floor: undefined,
     dateApproval: "2013-09-26",
     age: undefined,
     numParking: 22,
     phr: 0.23,
-
+    
     memo: undefined,
+
+    dateWritten: "2024-01-26",
   },
   {
-    name: "공릉2단지라이프",
-    category: "아파트",
+    category: "매매",
+    rating: 5,
 
+    name: "공릉2단지라이프",
+    categoryEstate: "아파트",
     jibunAddress: "서울시 노원구 공릉동 707",
     roadAddress: "서울시 노원구 동일로198가길 149",
 
@@ -130,16 +140,16 @@ const data = [
     wolsePriceMax: "1000/50",
     jmr: 55,
 
-    dateWritten: "2024-01-26",
-
     numHousehold: 660,
     floor: undefined,
     dateApproval: "2013-09-26",
     age: undefined,
     numParking: 257,
     phr: 0.38,
-
+    
     memo: undefined,
+
+    dateWritten: "2024-01-26",
   },
 ];
 
